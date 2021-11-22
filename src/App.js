5ea6a94ff229d79c09595f35472b5e38671acada
Natasha <njohnson533@gmail.com>
2021-11-22 12:28:08 -0500

    added components folder and styles folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
-import "./App.css";
 
-function App() {
-	return <div className="App">hello world</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello there
+      </div>
+    )
+  }
 }
 
 export default App;
