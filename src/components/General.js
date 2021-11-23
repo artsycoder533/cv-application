43150f0a076a4acabc3c39185716d4102c7aaffb
Natasha <njohnson533@gmail.com>
2021-11-23 00:53:33 -0500

    added event handlers and methods for form inputs

--- src/components/General.js
+++ src/components/General.js
@@ -1,40 +1,14 @@
 import React, { Component } from 'react';
 import GeneralInfo from './GeneralInfo';
-import { FiCheckCircle } from "react-icons/fi";
-import { FiEdit3 } from "react-icons/fi";
+
 
 export default class General extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            name: "",
-            email: "",
-            phone: "",
-            portfolio: "",
-            linkedIn: "",
-            github: "",
-        }
-    }
-
-    getInfo = (e) => {
-        e.preventDefault();
-        console.log(e);
-        console.log(e.target)
-    }
     render() {
         return (
             <div className="general">
-                <form onSubmit={this.getInfo} action="">
-                    <h2>General Info</h2>
-                    <GeneralInfo />
-                    <button>
-                        Edit <FiEdit3/>
-                    </button>
-                    <button type="submit">
-                        Submit Section <FiCheckCircle />
-                    </button>
-                </form>
+                <h2>General Info</h2>
+                <GeneralInfo />
             </div>
         );
     }
--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-
+import { FiCheckCircle } from "react-icons/fi";
+import { FiEdit3 } from "react-icons/fi";
 
 export default class GeneralInfo extends Component {
-    render() {
-        return (
-            <div className="general__info">
-                <label htmlFor="name">Name: </label>
-                <input type="text" />
-                <label htmlFor="email">Email: </label>
-                <input type="email" />
-                <br />
-                <label htmlFor="phone">Phone Number: </label>
-                <input type="telephone" />
-                <label htmlFor="portfolio">Portfolio: </label>
-                <input type="url" name="" id="" />
-                <br />
-                <label htmlFor="linkedIn">LinkedIn: </label>
-                <input type="url" />
-                <label htmlFor="github">Github: </label>
-                <input type="url" />
-            </div>
-        )
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			name: "",
+			email: "",
+			phone: "",
+			portfolio: "",
+			linkedIn: "",
+			github: "",
+		};
+	}
+
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        //form validation
+        //reset form
+        console.log(this.state);
+        e.target.reset();
     }
-}
+
+	render() {
+		return (
+			<div className="general__info">
+				<form action="" onSubmit={this.handleSubmit}>
+					<label htmlFor="name">Name: </label>
+					<input onChange={this.handleInput} name="name" type="text" />
+					<label htmlFor="email">Email: </label>
+					<input onChange={this.handleInput} name="email" type="email" />
+					<br />
+					<label htmlFor="phone">Phone Number: </label>
+					<input onChange={this.handleInput} name="phone" type="telephone" />
+					<label htmlFor="portfolio">Portfolio: </label>
+					<input
+						onChange={this.handleInput}
+						name="portfolio"
+						type="url"
+						id=""
+					/>
+					<br />
+					<label htmlFor="linkedIn">LinkedIn: </label>
+					<input onChange={this.handleInput} name="linkedIn" type="url" />
+					<label htmlFor="github">Github: </label>
+                    <input onChange={this.handleInput} name="github" type="url" />
+					<button>
+						Edit <FiEdit3 />
+					</button>
+					<button type="submit">
+						Submit Section <FiCheckCircle />
+					</button>
+				</form>
+			</div>
+		);
+	}
+};
+
+
+
